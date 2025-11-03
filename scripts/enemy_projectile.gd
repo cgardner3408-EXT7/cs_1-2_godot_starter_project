3a6c3feb9e12a0d036ab48902e51d0283b0d51b6
aansell <aansell@pps.net>
2025-11-03 08:22:26 -0800

    small changes

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,7 +1,17 @@
 extends Area2D
 
+var direction
+var speed = 300
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
+func _physics_process(_delta):
+	position += direction * speed * _delta
+
 func _on_body_entered(body):
 	pass
+	
+func set_direction(target):
+	
+	direction = position.direction_to(target)
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var projectile_original = preload("res://scenes/enemy_projectile.tscn")
 
 func _ready():
 	
@@ -13,9 +14,26 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	
+	if body.name == "Player":
+		shoot(body)
+		
 	pass # Replace with function body.
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	pass # Replace with function body.
+
+func shoot(body):
+	# TODO: Create a new projectile instance
+	var projectile_clone = projectile_original.instantiate()
+	
+	# TODO: Set projectile position to player position
+	projectile_clone.global_position = position
+	
+	# TODO: Set projectile direction using facing variable
+	projectile_clone.set_direction(body.position)
+	
+	# TODO: Add projectile to the game world
+	get_tree().get_root().add_child(projectile_clone)
+
+	pass
